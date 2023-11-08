dde8d906eed94ffe4029d09e4b45f70d80eb17ca
Maksym Shchyhol <drayv.mj12@gmail.com>
2023-11-08 21:29:29 +0100

    Update resume

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,65 +2,48 @@ import '../index.scss'
 import '../Styles/main.scss'
 import './Contact.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell } from '@fortawesome/free-regular-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faBuilding } from '@fortawesome/free-regular-svg-icons'
 import Analytics from '../Components/Analytics.js'
 
 function Contact() {
-    return (
-      <div id="main">
-        <Analytics/>
-        <div className="container is-max-desktop">
-          <div className="is-primary">
-            <section className="hero">
-              <div className="hero-body">
-                <p className="title">
-                  Maksym Shchyhol
-                </p>
-                <p className="subtitle">
-                  Contact me
-                </p>
-              </div>
-            </section>
-            <div className="main-text">
-              <div>
-                <span className="icon-text">
-                  <span className="icon">
-                    <FontAwesomeIcon icon={faBell} />
-                  </span>
-                  <span>Ukraine:</span>&nbsp;<a href="tel:+380938985489">+380938985489</a>
+  return (
+    <div id="main">
+      <Analytics />
+      <div className="container is-max-desktop">
+        <div className="is-primary">
+          <section className="hero">
+            <div className="hero-body">
+              <p className="title">
+                Maksym Shchyhol
+              </p>
+              <p className="subtitle">
+                Contact me
+              </p>
+            </div>
+          </section>
+          <div className="main-text">
+            <div>
+              <span className="icon-text">
+                <span className="icon">
+                  <FontAwesomeIcon icon={faEnvelope} />
                 </span>
-              </div>
-              <div>
-                <span className="icon-text">
-                  <span className="icon">
-                    <FontAwesomeIcon icon={faBell} />
-                  </span>
-                  <span>USA:</span>&nbsp;<a href="tel:+16694994143">+16694994143</a>
+                <a href="mailto: maksymshchyhol@gmail.com">maksymshchyhol@gmail.com</a>
+              </span>
+            </div>
+            <div>
+              <span className="icon-text">
+                <span className="icon">
+                  <FontAwesomeIcon icon={faBuilding} />
                 </span>
-              </div>
-              <div>
-                <span className="icon-text">
-                  <span className="icon">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                  </span>
-                  <a href="mailto: maksymshchyhol@gmail.com">maksymshchyhol@gmail.com</a>
-                </span>
-              </div>
-              <div>
-                <span className="icon-text">
-                  <span className="icon">
-                    <FontAwesomeIcon icon={faBuilding} />
-                  </span>
-                  <a href='https://www.linkedin.com/in/shchyhol/'>Linkedin</a>
-                </span>
-              </div>
+                <a href='https://www.linkedin.com/in/shchyhol/'>Linkedin</a>
+              </span>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Contact;
+export default Contact;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,11 +6,11 @@ import Years from '../Components/Years.js'
 import Analytics from '../Components/Analytics.js'
 
 function Home() {
-    return (
-      <div id="main">
-        <Analytics/>
-        <div className="container is-max-desktop">
-          <div className="is-primary">
+  return (
+    <div id="main">
+      <Analytics />
+      <div className="container is-max-desktop">
+        <div className="is-primary">
           <section className="hero">
             <div className="hero-body">
               <p className="title">
@@ -23,18 +23,18 @@ function Home() {
           </section>
           <div className="main-text">
             <span>
-<p>Hi, my name is <b>Maksym</b> and I am a software developer with{' '}<Years className="full-stack-years" digits="8" startDate="Sep 1, 2008"/>{' '}years of experience in the field.
-I have a strong background in full-stack development, with over {' '}<Years className="developer-years" digits="0"startDate="Oct 1, 2015" />{' '}years in this area, and have held leadership roles as a team lead and software architect.</p>
-<p>I am originally from Ukraine and have lived and worked in Malta, where I gained valuable experience working with a diverse team and learning about new technologies and programming languages.</p>
-<p>My passion is software development using the <b>.NET</b> stack and <b>JavaScript</b>, including front-end frameworks such as <b>Angular</b> and <b>React</b>. I am highly skilled in modern programming languages, applications, infrastructure, and tools such as Kubernetes, Docker, SQL, Redis, Elasticsearch, queuing systems, and more.</p>
-<p>I am always looking for opportunities to learn and grow as a developer. I am a fast learner and have a strong work ethic. I am a team player and enjoy working with others to solve problems and create innovative solutions.</p>
-<p>Please feel free to check out <b><Link to="/skills">my skills</Link></b> or <b><Link to="/contact">contact me</Link></b> for further information. I am confident that by working together, we can make a positive impact in the world. 💙💛</p>
+              <p>Hi, my name is <b>Maksym</b>, and I've been in the software development for{' '}<Years className="full-stack-years" digits="8" startDate="Sep 1, 2008" />{' '}years.</p>
+              <p>While my expertise predominantly centers on web development, with a particular emphasis on highly efficient back-end solutions, my experience also extends to desktop and mobile development, team leadership, and software architecture design.</p>
+              <p>My key areas of expertise spin around <b>.NET/C#, JavaScript, HTML, CSS</b>, as well as front-end frameworks like <b>Angular and React.</b></p>
+              <p>Additionally, I have a strong passion for infrastructure management and I've got my hands on all aspects needed to design, develop, host, and support projects of varying scales from the ground up.</p>
+              <p>I am always looking for opportunities to learn and grow and enjoy working with others to solve problems and create innovative solutions.</p>
+              <p>Please feel free to check out <b><Link to="/skills">my skills</Link></b> or <b><Link to="/contact">contact me</Link></b> for further information. I am confident that by working together, we can make a positive impact in the world. 💙💛</p>
             </span>
           </div>
         </div>
       </div>
     </div>
-    );
-  }
-  
-  export default Home;
+  );
+}
+
+export default Home;
