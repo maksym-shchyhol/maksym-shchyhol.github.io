0d3a35863759f77ddd5e3fa5a44bfc53e2f920e8
Maksym Shchyhol <drayv.mj12@gmail.com>
2023-11-09 01:58:38 +0100

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -24,11 +24,11 @@ function Home() {
           <div className="main-text">
             <span>
               <p>Hi, my name is <b>Maksym</b>, and I've been in the software development for{' '}<Years className="full-stack-years" digits="8" startDate="Sep 1, 2008" />{' '}years.</p>
-              <p>While my expertise predominantly centers on web development, with a particular emphasis on highly efficient back-end solutions, my experience also extends to desktop and mobile development, team leadership, and software architecture design.</p>
-              <p>My key areas of expertise spin around <b>.NET/C#, JavaScript, HTML, CSS</b>, as well as front-end frameworks like <b>Angular and React.</b></p>
+              <p>While my focus predominantly centers on web development, with a particular emphasis on highly efficient back-end solutions, my experience also extends to desktop and mobile development, team leadership, and software architecture design.</p>
+              <p>The key areas of my expertise spin around <b>.NET/C#, JavaScript, HTML, CSS</b>, as well as front-end frameworks like <b>Angular and React.</b></p>
               <p>Additionally, I have a strong passion for infrastructure management and I've got my hands on all aspects needed to design, develop, host, and support projects of varying scales from the ground up.</p>
-              <p>I am always looking for opportunities to learn and grow and enjoy working with others to solve problems and create innovative solutions.</p>
-              <p>Please feel free to check out <b><Link to="/skills">my skills</Link></b> or <b><Link to="/contact">contact me</Link></b> for further information. I am confident that by working together, we can make a positive impact in the world. 💙💛</p>
+              <p>I am always looking for new opportunities to learn and grow, and I enjoy working with others to solve problems and create innovative solutions.</p>
+              <p>Feel free to check out <b><Link to="/skills">my skills</Link></b> or <b><Link to="/contact">contact me</Link></b> for further information. I am confident that by working together, we can make a positive impact in the world. 💙💛</p>
             </span>
           </div>
         </div>
